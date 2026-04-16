ece20516ef20296987ae15f1b96ad2cdb8dad580
paglalarka976127-spec <paglalarka976127@gmail.com>
2026-04-16 11:08:41 +0530

    Create hello.js

---
+++ Priyansh/commands/hello.js
@@ -0,0 +1,28 @@
+ const fs = require("fs");
+module.exports.config = {
+	name: "hello",
+    version: "1.0.1",
+	hasPermssion: 0,
+	credits: "Arun", 
+	description: "no prefix",
+	commandCategory: "No command marks needed",
+	usages: "...",
+    cooldowns: 100, 
+};
+
+module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
+	var { threadID, messageID } = event;
+	let react = event.body.toLowerCase();
+	if(react.includes("hello") ||
+react.includes("hi")) {
+		var msg = {
+				body: "",
+				attachment: fs.createReadStream(__dirname + `/noprefix/hello.mp3`)
+			}
+			api.sendMessage(msg, threadID, messageID);
+    api.setMessageReaction("😜", event.messageID, (err) => {}, true)
+		}
+	}
+	module.exports.run = function({ api, event, client, __GLOBAL }) {
+
+  }
